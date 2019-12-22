f75ea99b1402aae31102e85094fdcf4640bdf6b7
George Tsakiridis <georgetsak321@gmail.com>
2019-12-22 05:39:52 +0200

    Finished Board class, made some progress on the GUI

--- src/com/georgetsak/sorry/controller/Controller.java
+++ src/com/georgetsak/sorry/controller/Controller.java
@@ -131,4 +131,8 @@ public class Controller {
 
     }
 
+    public static void main(String[] args) {
+        new Controller();
+    }
+
 }
--- src/com/georgetsak/sorry/viewer/View.java
+++ src/com/georgetsak/sorry/viewer/View.java
@@ -3,18 +3,21 @@ package com.georgetsak.sorry.viewer;
 import com.georgetsak.sorry.model.Color;
 import com.georgetsak.sorry.model.square.Square;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class View {
 
-    private Image imageBackground;
-    private Image imageBoard;
-
     private JPanel gamePanel;
     private JPanel optionsPanel;
+
+    private Image squareImage;
 
     private JButton newGameButton;
     private JButton exitButton;
@@ -33,6 +36,59 @@ public class View {
      * <b>Postcondition</b> a View is created and window along with its inner main components like panels are initialized and displayed
      */
     public View(){
+        JFrame frame = new JFrame("Sorry! 4186");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(1024, 605);
+
+        Image imageBackground = null;
+        Image imageBoard = null;
+        Image imageLogo = null;
+        squareImage = null;
+
+        try {
+            imageBackground = ImageIO.read(new File("src/assets/background.jpg"));
+            imageBoard = ImageIO.read(new File("src/assets/board.jpg"));
+            imageLogo = ImageIO.read(new File("src/assets/sorry_logo.png"));
+            squareImage = ImageIO.read(new File("src/assets/square.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(imageBackground == null || imageBoard == null || imageLogo == null){
+            System.err.println("Could not load images. Aborting...");
+            System.exit(-1);
+        }
+
+        JLabel background = new JLabel(new ImageIcon(imageBackground));
+        JLabel board = new JLabel(new ImageIcon(imageBoard));
+        JLabel sorryLogo = new JLabel(new ImageIcon(imageLogo));
+
+        JPanel mainPanel = new JPanel(null);
+        gamePanel = new JPanel(null);
+        optionsPanel = new JPanel(null);
+
+        gamePanel.setBounds(0, 0, 576, 576);
+
+        sorryLogo.setBounds(188, 188, 200, 200);
+
+        initBoardSquares();
+
+        gamePanel.add(sorryLogo);
+        gamePanel.add(board);
+        background.setBounds(0, 0, 1024, 576);
+        board.setBounds(0, 0, 576, 576);
+
+        initGui();
+
+        mainPanel.add(gamePanel);
+        mainPanel.add(background);
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
+
+        while(true);
+        //TODO initiate images and panels
     }
 
     /**
@@ -40,6 +96,27 @@ public class View {
      * <b>Postcondition</b> all buttons but square buttons are initialized and displayed into the gui
      */
     public void initGui(){
+        //TODO initiate all buttons but squares
+
+        for(int i = 0; i < 16; i++){
+            JButton test = new JButton(new ImageIcon(squareImage));
+            JButton test2 = new JButton(new ImageIcon(squareImage));
+
+            test.setBounds(i*36 + 1, 0, 34, 34);
+            test2.setBounds(i*36 + 1, 540, 34, 34);
+            gamePanel.add(test);
+            gamePanel.add(test2);
+        }
+
+        for(int i = 0; i < 14; i++){
+            JButton test = new JButton(new ImageIcon(squareImage));
+            JButton test2 = new JButton(new ImageIcon(squareImage));
+
+            test.setBounds(1, 36 + i*36, 34, 34);
+            test2.setBounds(541, 36 + i*36, 34, 34);
+            gamePanel.add(test);
+            gamePanel.add(test2);
+        }
 
     }
 
@@ -48,7 +125,7 @@ public class View {
      * <b>Postcondition</b> square buttons are initialized and displayed into gui
      */
     public void initBoardSquares(){
-
+        //TODO initiate squares
     }
 
     /**
